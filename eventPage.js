efb3649bfae70bd45f59f4d9be68c4ba8d91fead
Halil  Saltik <38754336+halilbsaltik@users.noreply.github.com>
2023-07-26 08:05:15 +0300

    Edevlet Extension
    Edevlet Automatic Login

---
+++ eventPage.js
@@ -0,0 +1,3 @@
+function textString(){
+    return "aaaa";
+}
---
+++ popup.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('#hs').click(function(){
+        window.open("https://giris.turkiye.gov.tr/Giris/gir");
+    });
+
+    $('#tb').click(function(){
+          window.open("https://www.turkiye.gov.tr/");
+    });
+});
+
+
